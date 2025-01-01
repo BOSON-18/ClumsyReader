c1760105da5d701917da3e7cd3811531b12f7754
BOSON-18 <deeshankbatra663@gmail.com>
2025-01-02 01:29:49 +0530

    Service EKY ENV

--- firsebaseAdmin.ts
+++ firsebaseAdmin.ts
@@ -1,9 +1,12 @@
 import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
-const serviceKey = require("./service_key.json");
+// const serviceKey = require("./service_key.json");
 
-
+const serviceKey = process.env.SERVICE_KEY_JSON;
+if(!serviceKey){
+    throw new Error("SERVICE_KEY_JSON is not set");
+}
 let app :App;
 if(getApps().length===0){
     app =initializeApp({
