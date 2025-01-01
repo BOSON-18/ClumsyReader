b8e6cbc9506376ce5acc21470c5e67271dd7f3eb
BOSON-18 <deeshankbatra663@gmail.com>
2025-01-02 01:40:43 +0530

    Deploy hoja - Jai mata di

--- src/app/dashboard/files/[id]/page.tsx
+++ src/app/dashboard/files/[id]/page.tsx
@@ -4,35 +4,38 @@ import { adminDb } from '../../../../../firsebaseAdmin'
 import PdfView from '@/components/global/PdfView'
 import Chat from '@/components/global/Chat'
 
-
 type Props = {
-    params:{id:string}
+    params: { id: string }
 }
 
-const ChatToFilePage =async ({params}: Props) => {
-    const {id}=await  params
-    auth.protect();
-    const {userId}=await auth();
+const ChatToFilePage = async ({ params }: Props) => {
+    const { id } = params; // no need to await params
 
-    const ref= await adminDb.collection("users").doc(userId!).collection("files").doc(id).get();
-    // //console.log("ref",ref);
-    // const url= ref.data()?.downloadurl?.stringValue;
-    const url = (ref as any)._fieldsProto.downloadUrl.stringValue;    //console.log("url",url);
-  return (
-    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
-    {/* Right */}
-    <div className="col-span-5 lg:col-span-2 overflow-y-auto">
-      {/* Chat */}
-     <Chat id={id} />
-    </div>
+    auth.protect(); // Protect the route
 
-    {/* Left */}
-    <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
-      {/* PDFView */}
-      <PdfView url={url} />
-    </div>
-  </div>
-  )
+    const { userId } = await auth(); // Use the userId from Clerk's auth
+
+    // Fetch the file from Firestore using the userId and id
+    const ref = await adminDb.collection("users").doc(userId!).collection("files").doc(id).get();
+
+    // Extract the download URL
+    const url = (ref as any)._fieldsProto.downloadUrl.stringValue;
+
+    return (
+        <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+            {/* Right */}
+            <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+                {/* Chat */}
+                <Chat id={id} />
+            </div>
+
+            {/* Left */}
+            <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+                {/* PDFView */}
+                <PdfView url={url} />
+            </div>
+        </div>
+    )
 }
 
-export default ChatToFilePage
+export default ChatToFilePage;
