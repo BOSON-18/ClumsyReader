140c71baa62fc98789d1ac511073e597cf7f2d93
BOSON-18 <deeshankbatra663@gmail.com>
2025-01-02 02:31:22 +0530

    hogya run build

--- firsebaseAdmin.ts
+++ firsebaseAdmin.ts
@@ -1,21 +1,33 @@
-import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
+import { initializeApp, getApps, App, getApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
-// const serviceKey = require("./service_key.json");
+import * as dotenv from "dotenv";
 
-const serviceKey = process.env.SERVICE_KEY_JSON;
-if(!serviceKey){
-    throw new Error("SERVICE_KEY_JSON is not set");
-}
-let app :App;
-if(getApps().length===0){
-    app =initializeApp({
+dotenv.config();
+
+const serviceKey:any = {
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Convert escaped newlines
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
+
+let app: App;
+if (getApps().length === 0) {
+    app = initializeApp({
         credential: cert(serviceKey),
-    })
-}else{
-    app=getApp();
+    });
+} else {
+    app = getApp();
 }
 
-const adminDb=getFirestore(app);
+const adminDb = getFirestore(app);
 
-export {app as adminApp,adminDb};
+export { app as adminApp, adminDb };
--- src/app/dashboard/files/[id]/page.tsx
+++ src/app/dashboard/files/[id]/page.tsx
@@ -1,37 +1,42 @@
-import { auth } from '@clerk/nextjs/server'
-import React from 'react'
-import { adminDb } from '../../../../../firsebaseAdmin'
-import PdfView from '@/components/global/PdfView'
-import Chat from '@/components/global/Chat'
+import { auth } from '@clerk/nextjs/server';
+import React from 'react';
+import { adminDb } from '../../../../../firsebaseAdmin';
+import PdfView from '@/components/global/PdfView';
+import Chat from '@/components/global/Chat';
 
-type Props = {
-    params: { id: string }
+type Params = Promise<{ id: string }>; // Defining the type for params
+
+async function ChatToFilePage({
+  params,
+}: {
+  params: Params;
+}): Promise<React.ReactNode> { // Explicit return type
+  // Await the params to get the `id`
+  const { id } = await params; 
+
+  // Authentication
+  auth.protect();
+  const { userId } = await auth();
+
+  // Fetch the file reference from Firebase
+  const ref = await adminDb.collection("users").doc(userId!).collection("files").doc(id).get();
+  const url = (ref as any)._fieldsProto.downloadUrl.stringValue; // Extract download URL
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* Right */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/* Chat */}
+        <Chat id={id} />
+      </div>
+
+      {/* Left */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        {/* PDFView */}
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 
-// No need to `await` the `params` since it's already available
-const ChatToFilePage = async ({ params }: Props) => {
-    const { id } = params; // Directly use params here, no need to await it
-    auth.protect();
-    const { userId } = await auth();
-
-    const ref = await adminDb.collection("users").doc(userId!).collection("files").doc(id).get();
-    const url = (ref as any)._fieldsProto.downloadUrl.stringValue; // Access URL
-
-    return (
-        <div className="grid lg:grid-cols-5 h-full overflow-hidden">
-            {/* Right */}
-            <div className="col-span-5 lg:col-span-2 overflow-y-auto">
-                {/* Chat */}
-                <Chat id={id} />
-            </div>
-
-            {/* Left */}
-            <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
-                {/* PDFView */}
-                <PdfView url={url} />
-            </div>
-        </div>
-    )
-}
-
-export default ChatToFilePage
+export default ChatToFilePage;
