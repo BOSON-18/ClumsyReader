8f342e1cec01168d8512f98ae87521cde041031b
BOSON-18 <deeshankbatra663@gmail.com>
2025-01-02 01:35:40 +0530

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
+    },
+  },
 ];
-
 export default eslintConfig;
+
